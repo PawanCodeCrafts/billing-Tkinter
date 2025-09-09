5f30901c0d60999b0c26c981a648463d8e730282
pawancodecrafts <kpawanpreet2003@gmail.com>
2025-09-09 13:49:39 +0530

    grid

--- billing.py
+++ billing.py
@@ -1,20 +1,46 @@
 #Tkinter Python ka built-in library hai jo GUI (Graphical User Interface) banane ke liye use hota hai.
 # importing libraries
-import tkinter as Tk
+import tkinter as tk
 
 # creating main application window
-root = Tk.Tk()
+root = tk.Tk()
 
+###############################################
+################# ROOT FUNCTIONS ##############
+###############################################
+
+
+#resizing the window (in pixels)
+root.geometry("300x300")
+#changing title of the window -- we can also change icon of the window
+root.title("Billing system")
+
+
+
+###############################################
+################# Label FUNCTIONS #############
+###############################################
 # make label
-a = Tk.Label(text="Hello, Tkinter!")
-# display label using pack (is se label ko display krte hain)
-a.pack()
+# a = tk.Label(text="Hello, Tkinter!")
+# # display label using pack (is se label ko display krte hain)
+# a.pack()
 
-b = Tk.Label(text="Welcome to Python programming.")
-b.pack()
+d = tk.Label(text="raman")
+d.grid(column=1, row=1)
 
-c = Tk.Label(text="I am Pawanpreet")
-c.pack()
+b = tk.Label(text="raman")
+b.grid(column=1, row=2)
 
+
+
+
+
+
+
+
+
+
+# no resizing
+root.resizable(True, False)
 # running the window
 root.mainloop()
