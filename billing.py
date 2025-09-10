a657aaef961602e797353fcaffd690ba2db8c2a8
pawancodecrafts <kpawanpreet2003@gmail.com>
2025-09-10 13:00:53 +0530

    button & entry

--- billing.py
+++ billing.py
@@ -2,48 +2,30 @@
 # importing libraries
 import tkinter as tk
 
+def sayHello():
+    print("Hello World")
+
 # creating main application window
 root = tk.Tk()
 
-###############################################
-################# ROOT FUNCTIONS ##############
-###############################################
 
 
-#resizing the window (in pixels)
+# buttons
+a = tk.Button(text="click me",command=sayHello)
+a.grid(column=1,row=1)
+
+c =tk.Label(text="enter data")
+c.grid(column=1,row=2)
+
+b = tk.Entry()
+b.grid(column=2,row=2)
+
+
+#resizing window (in pixels)
 root.geometry("300x300")
-#changing title of the window -- we can also change icon of the window
+
+#changing title 
 root.title("Billing system")
-
-
-
-###############################################
-################# Label FUNCTIONS #############
-###############################################
-# make label
-# a = tk.Label(text="Hello, Tkinter!")
-# # display label using pack (is se label ko display krte hain)
-# a.pack()
-
-################# GRID #######################
-# d = tk.Label(text="raman")
-# d.grid(column=1, row=1)
-# b = tk.Label(text="raman")
-# b.grid(column=1, row=2)
-
-####################### place ######################
-
-a = tk.Label(text="billing system", font=("Arial", 24), fg="blue" , bg="yellow")
-a.place(x=100, y=10)
-
-b= tk.Label(text="product")
-b.place(x=100, y=50)
-
-
-
-
-
-
 # no resizing
 root.resizable(True, False)
 # running the window
